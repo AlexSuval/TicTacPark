e20b4079798150512f8a51c9398b4ea2e7e7de71
AlexSuval <alexsuval@gmail.com>
2015-12-21 18:56:15 +0100

    Se implementa el icono de refresco para la pantalla de Alarma y gasto.

--- app/src/main/java/com/alex/tictacpark/fragments/AlarmaFragment.java
+++ app/src/main/java/com/alex/tictacpark/fragments/AlarmaFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +15,7 @@ import com.alex.tictacpark.R;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.logging.Handler;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +28,7 @@ import java.util.Calendar;
 public class AlarmaFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_SECTION_NUMBER = "section_number";
+    private  SwipeRefreshLayout srl;
 
     /**
      * Use this factory method to create a new instance of
@@ -51,10 +54,23 @@ public class AlarmaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_alarma, container, false);
+        final View v = inflater.inflate(R.layout.fragment_alarma, container, false);
+
+        //Se configura el TimePicker en formato 24h
         TimePicker tp =(TimePicker)v.findViewById(R.id.timePicker);
         tp.setIs24HourView(true);
-        RellenarTextView(v);
+
+        //Se rellenan los TextView
+        RellenarTextView(v,false);
+
+        //Se implementa el icono de refresco actualizando los datos
+        srl = (SwipeRefreshLayout)v.findViewById(R.id.swipe_refresh_layout);
+        srl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener(){
+            @Override
+        public void onRefresh(){
+                RellenarTextView(v,true);
+            }
+        });
         return v;
     }
 
@@ -73,17 +89,32 @@ public class AlarmaFragment extends Fragment {
         public void onFragmentInteraction(Uri uri);
     }
 
-    private void RellenarTextView(View v)
-    {
-        TextView tiempo = (TextView)v.findViewById(R.id.tv_respuesta_tiempo);
-        TextView gasto = (TextView)v.findViewById(R.id.tv_respuesta_gasto);
-        TextView restante = (TextView)v.findViewById(R.id.tv_respuesta_restante);
-        TextView actualizacion = (TextView)v.findViewById(R.id.tv_respuesta_actualizacion);
+    private void RellenarTextView(View v, boolean actualizar) {
+        TextView tiempo = (TextView) v.findViewById(R.id.tv_respuesta_tiempo);
+        TextView gasto = (TextView) v.findViewById(R.id.tv_respuesta_gasto);
+        TextView restante = (TextView) v.findViewById(R.id.tv_respuesta_restante);
+        TextView actualizacion = (TextView) v.findViewById(R.id.tv_respuesta_actualizacion);
 
+        /*if (actualizar) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    // Obtener hora actual para completar el TextView actualización
+                    Calendar cal = Calendar.getInstance();
+                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+                    actualizacion.setText(sdf.format(cal.getTime()));
+                }
+            }
+        }
+*/
         // Obtener hora actual para completar el TextView actualización
-        Calendar cal= Calendar.getInstance();
-        SimpleDateFormat sdf=new SimpleDateFormat("HH:mm:ss");
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
         actualizacion.setText(sdf.format(cal.getTime()));
-    }
 
+        if(actualizar)
+        {
+            srl.setRefreshing(false); //Una vez actualizado se oculta el icono de refresco
+        }
+    }
 }
